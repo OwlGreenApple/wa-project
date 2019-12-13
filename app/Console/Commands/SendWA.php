e0989bb955e257338a5f442baafd66f573f078d3
rizky redjo <celebgramme.dev@gmail.com>
2019-12-13 11:32:28 +0700

    Fixing

--- app/Console/Commands/SendWA.php
+++ app/Console/Commands/SendWA.php
@@ -14,6 +14,7 @@ use Carbon\Carbon;
 use App\User;
 use App\Sender;
 use App\Http\Controllers\ApiController;
+use DB;
 
 class SendWA extends Command
 {
@@ -407,11 +408,12 @@ class SendWA extends Command
                             ['reminder_customers.status','=',0],
                             ['lists.is_event','=',0],
                             ['customers.created_at','<=',$current_time->toDateTimeString()],
-                            ['myDays','>=','reminders.days'],
-                            ])->rightJoin('reminders','reminder_customers.reminder_id','=','reminders.id')
+                            ])
+                            ->whereRaw('DATEDIFF(customers.created_at,now()) >= reminders.days')
+                            ->rightJoin('reminders','reminder_customers.reminder_id','=','reminders.id')
                             ->join('lists','lists.id','=','reminders.list_id')
                             ->leftJoin('customers','customers.id','=','reminder_customers.customer_id')
-                            ->select('reminder_customers.id AS rcs_id','reminder_customers.status AS rc_st','reminder_customers.sender_id','reminders.package','reminders.days','reminders.message','reminders.subject','reminders.mail','customers.created_at AS cstreg','customers.wa_number','customers.name','customers.list_id AS clid','customers.email','customers.is_pay',DB::raw("DATEDIFF(customers.created_at,now())AS myDays"))
+                            ->select('reminder_customers.id AS rcs_id','reminder_customers.status AS rc_st','reminder_customers.sender_id','reminders.package','reminders.days','reminders.message','reminders.subject','reminders.mail','customers.created_at AS cstreg','customers.wa_number','customers.name','customers.list_id AS clid','customers.email','customers.is_pay')
                           //->take($count)
                           ->get();
 
