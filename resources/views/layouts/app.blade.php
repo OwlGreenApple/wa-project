760045afab7b031b82d1cdbd6cca22af645ec7ad
Gunardi Omnifluencer <gunardi.omnifluencer@gmail.com>
2020-01-24 13:38:24 +0700

    prototype-24-jan-2020

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,9 @@
     <!-- Font Awesome 4.7 -->
     <link href="{{ asset('/assets/Font-Awesome-4.7.0/css/font-awesome.min.css') }}" rel="stylesheet">
 
+    <!-- Icomoon -->
+    <link href="{{ asset('/assets/icomoon/icomoon.css') }}" rel="stylesheet" />
+
      <!-- Emoji -->
     <link href="{{ asset('/assets/emoji/css/emojionearea.min.css') }}" rel="stylesheet"> 
     <script type="text/javascript" src="{{ asset('/assets/emoji/js/prettify.js') }}"></script>
@@ -54,6 +57,9 @@
     <!-- Clipboard -->
     <script type="text/javascript" src="{{ asset('/assets/clipboard.js-master/clipboard.min.js') }}"></script> 
 
+    <!-- Main Styles -->
+    <link href="{{ asset('/assets/css/main.css') }}" rel="stylesheet">
+
 </head>
 <body>
 
@@ -64,10 +70,10 @@
     </div>
 
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-telegram header-tel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img class="logo" src="{{asset('assets/img/logo.png')}}" />
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -75,12 +81,7 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav">
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
@@ -92,6 +93,25 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link {{ (request()->is('home')) ? 'active' : '' }}" href="{{ route('home') }}">Create</a>
+                            </li> 
+
+                            <li class="nav-item">
+                                <a class="nav-link">Audiences</a>
+                            </li> 
+
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Campaigns <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="nav-link">Audiences</a>
+                                    <a class="nav-link">Campaigns</a>
+                                </div>
+                            </li> 
+                            <!--
                             <li class="nav-item">
                                 <a class="nav-link {{ (request()->is('home')) ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
                             </li> 
@@ -130,8 +150,26 @@
                                     </form>
                                 </div>
                             </li>
+                          -->
                         @endguest
                     </ul>
+                    <ul class="navbar-nav mr-auto"><!-- separator --></ul>
+                     <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item mr-3">
+                           Hi,
+                        </li>
+                        <li class="nav-item mr-3">
+                           <select class="form-control-sm tel-color" name="">
+                             <option>08523193113</option>
+                           </select>
+                        </li>
+                         <li class="nav-item cog-pos">
+                           <span class="icon-cog"></span>
+                        </li>
+                    </ul>
+
+
                 </div>
             </div>
         </nav>
