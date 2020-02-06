89a15c1c281f77088e909bbc75359dbe84c5608c
rizky redjo <celebgramme.dev@gmail.com>
2020-02-06 17:37:34 +0700

    setting

---
+++ app/Http/Controllers/SettingController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\File;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Imports\UsersImport;
+use App\User;
+
+class SettingController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function connect_phone()
+    {
+      $curl = curl_init();
+      $data = array(
+          'token'=> '0698a365aec87be50795ab07230d7df55df6eda532b81',
+          'phone_number'=>'+628123238793',
+          'authcode'=>'68677',
+          // 'phone_number'=>'+6287723238793',
+          // 'phone_number'=>'+6287855915535',
+          
+          'listname'=>'tdlib',
+          // tdlib tdlib2 tdlib3 tdlib5
+      );
+
+      curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://172.98.193.36/phptdlib/php_examples/auth-set-phone.php",
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => http_build_query($data),
+        CURLOPT_POST => 1,
+      ));
+
+      $response = curl_exec($curl);
+      $err = curl_error($curl);
+
+      curl_close($curl);
+
+      if ($err) {
+        echo "cURL Error #:" . $err;
+      } else {
+        echo $response."\n";
+        // print_r($response);
+        // return json_decode($response, true);
+      }
+    }
+
+/* end class HomeController */
+}
