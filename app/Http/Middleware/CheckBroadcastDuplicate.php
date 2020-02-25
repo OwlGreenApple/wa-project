c88cb3801e3441e440ac811e7cbae5921e178546
Gunardi Omnifluencer <gunardi.omnifluencer@gmail.com>
2020-02-25 17:02:49 +0700

    fix-database

--- app/Http/Middleware/CheckBroadcastDuplicate.php
+++ app/Http/Middleware/CheckBroadcastDuplicate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Validator;
 
 class CheckBroadcastDuplicate
 {
@@ -15,6 +16,31 @@ class CheckBroadcastDuplicate
      */
     public function handle($request, Closure $next)
     {
+        $campaign_name = $request->campaign_name;
+        $listid = $request->list_id;
+        $date_send = $request->date_send;
+        $hour = $request->hour;
+        $message = $request->message;
+
+        if(!empty($listid))
+        {
+          'list_id'=>['required'],
+        }
+
+        $rules = [
+          'campaign_name'=>['required','max:50'],
+          'date_send'=>['required','max:50'],
+        ];
+        $validator = Validator::make($request->all(),$rules);
+
+        if($validator->fails())
+        {
+            $error = $validator->errors();
+            return response()->json([
+              'campaign_name'=>$error->first('campaign_name'),
+              'success'=>0,
+            ]);
+        }
         return $next($request);
     }
 }
