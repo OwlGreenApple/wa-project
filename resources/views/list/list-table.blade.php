656c9456dfc5bd4bafc5aa3f6105de479076a503
Gunardi Omnifluencer <gunardi.omnifluencer@gmail.com>
2020-03-03 18:04:15 +0700

    fix-list-view

--- resources/views/list/list-table.blade.php
+++ resources/views/list/list-table.blade.php
@@ -6,40 +6,44 @@
 <!-- tab -->
 @if($lists->count() > 0)
   @foreach($lists as $rows)
-    <div class="bg-dashboard cardlist col-lg-12 row">
-      <div class="col-lg-4 pad-fix col-card">
-        <h5>{{$rows->label}}</h5>
-          <div class="link_wrap">Link From : {{env('APP_URL')}}{{$rows->name}}
-            <span>
-              <a data-link="{{env('APP_URL')}}{{ $rows->name }}" class="btn-copy icon-copy"></a>
-            </span>
-          </div>
+    <div class="bg-dashboard cardlist col-lg-12">
 
-          <div>Created On : {{Date("M d, Y", strtotime($rows->created_at))}}</div> <!--Jan 23, 2020  -->
-      </div>
+      <div class="row">
+        <div class="col-lg-4 col-card">
+          <h5>{{$rows->label}}</h5>
+            <div class="link_wrap">Link From : {{env('APP_URL')}}{{$rows->name}}
+              <span>
+                <a data-link="{{env('APP_URL')}}{{ $rows->name }}" class="btn-copy icon-copy"></a>
+              </span>
+            </div>
 
-      <div class="col-lg-3 pad-fix cardnumber">
-        <div class="big-number">
-          +<?php 
-            echo $listcontroller->newContact($rows->id);
-          ?>
+            <div>Created On : {{Date("M d, Y", strtotime($rows->created_at))}}</div> <!--Jan 23, 2020  -->
         </div>
-        <div class="contact">New Contacts</div>
-      </div> 
 
-      <div class="col-lg-3 pad-fix cardnumber">
-        <div class="big-number"><?php 
-            echo $listcontroller->contactList($rows->id);
-          ?></div>
-        <div class="contact">Contacts</div>
+        <div class="col-lg-3 pad-fix cardnumber">
+          <div class="big-number">
+            +<?php 
+              echo $listcontroller->newContact($rows->id);
+            ?>
+          </div>
+          <div class="contact">New Contacts</div>
+        </div> 
+
+        <div class="col-lg-3 pad-fix cardnumber">
+          <div class="big-number"><?php 
+              echo $listcontroller->contactList($rows->id);
+            ?></div>
+          <div class="contact">Contacts</div>
+        </div>
+
+        <div class="col-lg-2 pad-fix col-button">
+          <button type="button" id="{{ $rows->id }}" class="btn btn-warning btn-sm open-table"><span class="icon-eye"></span></button>
+          <a href="{{url('list-edit')}}/{{$rows->id}}" class="btn btn-edit btn-sm" target="_blank"><span class="icon-edit"></span></a>
+          <button id="{{$rows->id}}" type="button" class="btn btn-success btn-sm duplicate"><span class="icon-copy-text"></span></button>
+          <a type="button" id="{{$rows->id}}" class="btn btn-danger btn-sm del"><span class="icon-delete"></span></a>
+        </div> 
       </div>
 
-      <div class="col-lg-2 pad-fix col-button">
-        <button type="button" id="{{ $rows->id }}" class="btn btn-warning btn-sm open-table"><span class="icon-eye"></span></button>
-        <a href="{{url('list-edit')}}/{{$rows->id}}" class="btn btn-edit btn-sm" target="_blank"><span class="icon-edit"></span></a>
-        <button id="{{$rows->id}}" type="button" class="btn btn-success btn-sm duplicate"><span class="icon-copy-text"></span></button>
-        <a type="button" id="{{$rows->id}}" class="btn btn-danger btn-sm del"><span class="icon-delete"></span></a>
-      </div> 
     </div> 
   @endforeach
   @else
