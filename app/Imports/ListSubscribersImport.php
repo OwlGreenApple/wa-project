166eee75d0e8a7b899016a29f9b26110285af657
Gunardi Omnifluencer <gunardi.omnifluencer@gmail.com>
2020-03-30 11:03:58 +0700

    fix-import

--- app/Imports/ListSubscribersImport.php
+++ app/Imports/ListSubscribersImport.php
@@ -62,7 +62,7 @@ class ListSubscribersImport implements ToCollection,WithStartRow
 
         $rules = [
            '*.name'=> ['required'],
-           '*.phone'=> ['required','not_regex:/^[a-zA-z]*$/i','not_regex:/^0[0-9]*$/i'],
+           '*.phone'=> ['required','not_regex:/^0[0-9]*$/i'],
            // '*.phone'=> ['required_if:*.username,==,'.null.'',new TelegramNumber],
            '*.email'=> ['required','email'],
            //'*.username'=> ['required_if:*.phone,==,'.null.''],
