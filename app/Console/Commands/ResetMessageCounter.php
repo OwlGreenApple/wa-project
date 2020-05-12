f6ba9c3a9af2167b72d29862786f5c266831bee9
rizky redjo <celebgramme.dev@gmail.com>
2020-05-12 15:24:20 +0700

    fixing

--- app/Console/Commands/ResetMessageCounter.php
+++ app/Console/Commands/ResetMessageCounter.php
@@ -62,8 +62,8 @@ class ResetMessageCounter extends Command
 									// $additional_day = 180;
 									$type_package = explode("supervalue", $user->membership)[0];
 								}
-print_r($users);
-print_r($type_package);
+//print_r($users);
+echo $users->email.$users->membership.$type_package."\n";
 								$max_counter = 0;
 								if ($type_package=="1") {
 									$phoneNumber->max_counter_day=1000;
