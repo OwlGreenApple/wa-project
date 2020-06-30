f0473c070d1cbbd81bb81e69c867c9227300a83b
Gunardi Omnifluencer <gunardi.omnifluencer@gmail.com>
2020-06-30 20:17:37 +0700

    woowa-list-order

--- resources/views/admin/list-woowa/index.blade.php
+++ resources/views/admin/list-woowa/index.blade.php
@@ -28,11 +28,11 @@
                   responsive: true,
                   destroy: true,
                   columnDefs: [
-                    { "width": "5%", "targets": 0 }
-                    { "width": "25%", "targets": 1 }
-                    { "width": "25%", "targets": 2 }
-                    { "width": "10%", "targets": 3 }
-                    { "width": "10%", "targets": 4 }
+                    { "width": "5%", "targets": 0 },
+                    { "width": "25%", "targets": 1 },
+                    { "width": "25%", "targets": 2 },
+                    { "width": "10%", "targets": 3 },
+                    { "width": "10%", "targets": 4 },
                     { "width": "25%", "targets": 5 }
                   ],
                   "order": [],
