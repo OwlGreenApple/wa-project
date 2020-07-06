0b8c85d3b66521d1548148bed9789eb8c6446773
Gunardi Omnifluencer <gunardi.omnifluencer@gmail.com>
2020-07-06 09:21:31 +0700

    fix-font-plugins-ckeditor

--- resources/views/list/list-edit.blade.php
+++ resources/views/list/list-edit.blade.php
@@ -744,8 +744,8 @@ var _0x2799=['https://activrespon.com/dashboard/entry-google-form','fetch','appl
       allowedContent: true,
       filebrowserBrowseUrl: "{{ route('ckbrowse') }}",
       filebrowserUploadUrl: "{{ route('ckupload') }}",
-      // extraPlugins: ['font','videoembed'],
-      extraPlugins: ['uploadimage','image2','justify','colorbutton','videoembed'],
+      // extraPlugins: ['font'],
+      extraPlugins: ['uploadimage','image2','justify','colorbutton','videoembed','font'],
       removePlugins : 'image',
   };
 
@@ -754,8 +754,8 @@ var _0x2799=['https://activrespon.com/dashboard/entry-google-form','fetch','appl
 
   CKEDITOR.editorConfig = function( config ) {
       config.extraAllowedContent = true;
-      config.extraPlugins = 'uploadimage','image2','justify','colorbutton','videoembed';
-      // config.extraPlugins = 'font','videoembed';
+      config.extraPlugins = 'uploadimage','image2','justify','colorbutton','videoembed','font';
+      // config.extraPlugins = 'font',;
       config.removePlugins = 'image';
   };
 
