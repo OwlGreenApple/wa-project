3e333a513e8d8c177e20cb5276974a66fd188e23
rizky redjo <celebgramme.dev@gmail.com>
2020-07-14 11:41:42 +0700

    fixing

--- app/Http/Controllers/ApiWPController.php
+++ app/Http/Controllers/ApiWPController.php
@@ -19,6 +19,11 @@ use App\Message;
 
 class ApiWPController extends Controller
 {
+  /*
+    checkout-created = untuk order yang dibuat
+    checkout-completed = untuk order yang selesai
+    reminder = untuk pengingat  
+  */
     public function send_message_queue_system_WP_activtemplate(Request $request)
     {
       if ($request->key == "wpcallbackforwa" ) {
@@ -35,6 +40,35 @@ class ApiWPController extends Controller
 
         if(preg_match('/^[^62][0-9]*$/',$str)){
           $phone_number = preg_replace("/^[0-9]/", "+62", $str);
+        }
+        
+        if ($request->event == "checkout-completed"){
+          //list khusus activtemplate
+          $list = UserList::where('name',"3ha6ierm")->first();
+
+          if (!is_null($list)) {
+            $customer_phone = Customer::where([['list_id',$list->id],['telegram_number',$phone_number]])->first();
+
+            if(is_null($customer_phone))
+            {
+              $customer = new Customer ;
+              $customer->user_id = $list->user_id;
+              $customer->list_id = $list->id;
+              $customer->name = $request->name;
+              $customer->email = $request->email;
+              $customer->telegram_number = $phone_number;
+              $customer->is_pay= 0;
+              $customer->status = 1;
+              $customer->save();
+              $customer::create_link_unsubs($customer->id,$list->id);
+
+              $customerController = new CustomerController;
+              if ($list->is_secure) {
+                $ret = $customerController->sendListSecure($list->id,$customer->id,$request->name,$customer->user_id,$list->name,$phone_number);
+              }
+              $saveSubscriber = $customerController->addSubscriber($list->id,$customer->id,$customer->created_at,$customer->user_id);
+            }
+          }
         }
         
         $message_send = Message::create_message($phone_number,$request->content,env('REMINDER_PHONE_KEY'));
